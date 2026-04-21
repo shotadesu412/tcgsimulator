e44882b4d69bf6593da8c85bdfa14c5f431bacd5
aota441122 <f_shota412@icloud.com>
2026-04-21 20:02:45 +0900

    feat: manual card registration (civ/cost/text), deck search + in-deck sort, no-image card display

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "992025140"
+    serviceWorkerVersion: "352140594"
   }
 });
