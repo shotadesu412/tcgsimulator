2e1018f35ed90e67af6eede1c9e7f99df91289f8
aota441122 <f_shota412@icloud.com>
2026-04-22 19:15:12 +0900

    fix: full-screen card drag - remove conflicting onLongPress, fix rootNavigator pop

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2444923394"
+    serviceWorkerVersion: "136419586"
   }
 });
